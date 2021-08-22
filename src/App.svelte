0c825ed87dd30b2e553125cc83f5115bcc771ded
shinyaigeek <philispaxil@gmail.com>
2021-08-22 13:15:40 +0900

    Feature: add <Header />

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
+	import Header from "./components/Header.svelte";
+
+	
+
 	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<Header />
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
---
+++ src/components/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<main>
+	<h1>Shinyaigeek's GitHub Activity</h1>
+</main>
+
+<style>
+</style>
