37f9792c3dac9d2bd28d46b061a8dd8c6b46e5f3
shinyaigeek <philispaxil@gmail.com>
2021-08-24 19:06:49 +0900

    Chore: fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#080708",
+      white: "#faf5fa",
+      gray: "#C6C6C6",
+      indigo: "#4b0082",
+      red: "#F26052",
+      yellow: "#F2D544",
+    },
   },
   variants: {
     extend: {},
