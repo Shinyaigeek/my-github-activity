a0a078685c4081484b531d8d1b0b6e529436b301
shinyaigeek <philispaxil@gmail.com>
2021-08-24 16:45:22 +0900

    Feature: fetch GitHub Activities from api and arrange

--- api/getGitHubActivities.ts
+++ api/getGitHubActivities.ts
@@ -1,7 +1,27 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Octokit } from "@octokit/rest";
 
-export default (request: VercelRequest, response: VercelResponse) => {
-  const { name = 'World' } = request.query;
-  response.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
-  response.status(200).send(`Hello ${name}!`);
+const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_TOKEN,
+});
+
+const user = process.env.GITHUB_USERNAME;
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const { data } = await octokit.rest.search.issuesAndPullRequests({
+    q: `involves:${user} -user:${user} author:${user} is:public `,
+    per_page: 100,
+    page: 1,
+  });
+  const items = data.items.map(({ html_url, title, number }) => {
+    const [, , , org, repositoryName] = html_url.split("/");
+    const repository = `${org}/${repositoryName}`;
+    return {
+      repository,
+      url: html_url,
+      title,
+    };
+  });
+  response.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate");
+  response.status(200).json(items);
 };
