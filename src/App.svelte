c95dcb74522cc95a3b6a271f67b86d3548b77a50
shinyaigeek <philispaxil@gmail.com>
2021-08-24 16:54:02 +0900

    Feature: show my github activities in /

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,43 @@
 <script>
-	import Header from "./components/Header.svelte";
+  import Header from "./components/Header.svelte";
 
-	
-
-	export let name;
+  const getGitHubActivities = (async () => {
+    const response = await fetch("/api/getGitHubActivities");
+    return await response.json();
+  })();
 </script>
 
 <main>
-	<Header />
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Header />
+  {#await getGitHubActivities}
+    <p>...waiting</p>
+  {:then items}
+  {#each items as item}
+	<div>{item.repository}</div>
+  {/each}
+  {:catch error}
+    <p>An error occurred!</p>
+  {/await}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
