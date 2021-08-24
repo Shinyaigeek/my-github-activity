68911da305ee3f2a3f140f759a63cc9c05e81d57
shinyaigeek <philispaxil@gmail.com>
2021-08-24 19:45:19 +0900

    Feature: style header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <main>
-	<h1 class="text-red-100">Shinyaigeek's GitHub Activity</h1>
+	<h1 class="text-white bg-black text-3xl p-2">Shinyaigeek's GitHub Activity</h1>
 </main>
 
 <style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,36 @@ module.exports = {
       red: "#F26052",
       yellow: "#F2D544",
     },
+    screens: {
+      tablet: "640px",
+
+      laptop: "1024px",
+
+      desktop: "1280px",
+    },
+    spacing: {
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+    },
+    borderRadius: {
+      none: "0",
+      sm: ".125rem",
+      DEFAULT: ".25rem",
+      lg: ".5rem",
+      full: "9999px",
+    },
+    opacity: {
+      0: "0",
+      20: "0.2",
+      40: "0.4",
+      60: "0.6",
+      80: "0.8",
+      100: "1",
+    },
   },
   variants: {
     extend: {},
