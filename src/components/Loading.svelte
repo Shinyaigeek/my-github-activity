becb639b03b1b30c6b7861482370e1f9dab81a2f
shinyaigeek <philispaxil@gmail.com>
2021-08-25 09:55:49 +0900

    Update: <Loading /> dependes on parent size

---
+++ src/components/Loading.svelte
@@ -0,0 +1,21 @@
+<script></script>
+
+<div
+  class="loader ease-linear h-full w-full rounded-full border-4 border-t-4 border-gray"
+/>
+
+<style>
+  .loader {
+    border-top-color: #3498db;
+    animation: spinner 1.5s linear infinite;
+  }
+
+  @keyframes spinner {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
